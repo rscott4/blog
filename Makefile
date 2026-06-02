1faf56cebf5909a7a1df3f4e4e781585791a04e5
rscott <personal@rscott.uk>
2026-06-02 14:09:57 +0100

    index: Created json index generator

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 TFILES = $(wildcard *.typ)
 PFILES = $(patsubst %.typ,%.pdf,$(TFILES))
 HFILES = $(patsubst %.typ,%.html,$(TFILES))
+INDEX_JSON_FILE_PATH=index.json
 
-all: pdf html
+all: index pdf html
+
+index: $(TFILES)
+	./index.sh $<
 
 pdf: $(PFILES)
 
@@ -17,3 +21,4 @@ html: $(HFILES)
 clean:
 	rm -f $(PFILES)
 	rm -f $(HFILES)
+	rm -f $(INDEX_JSON_FILE_PATH)
---
+++ index.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+: ${INDEX_JSON_FILE_PATH:="./index.json"}
+
+json=
+json+="["
+for typ in $@; do
+    name="$(basename "${typ}" | sed 's/.typ$//g')"
+    mod_times="$(git log --follow --format=%ad --date=unix "${typ}")"
+    created="$(echo "${mod_times}" | tail -1)"
+    modified="$(echo "${mod_times}" | head -1)"
+    typ="./${typ}"
+    html="$(echo "${typ}" | sed 's/.typ$/.html/g')"
+    pdf="$(echo "${typ}" | sed 's/.typ$/.pdf/g')"
+    json+=$(jq --null-input \
+        --arg name "${name}" \
+        --arg created "${created}" \
+        --arg modified "${modified}" \
+        --arg typ "${typ}" \
+        --arg html "${html}" \
+        --arg pdf "${pdf}" \
+        "{ \
+        name: \$name, \
+        created: \$created, \
+        modified: \$modified, \
+        typ: \$typ, \
+        html: \$html, \
+        pdf: \$pdf \
+        }")
+    json+=","
+done
+[ "$#" != "0" ] && json="${json::-1}"
+json+="]"
+
+echo "${json}" | jq > "${INDEX_JSON_FILE_PATH}"
